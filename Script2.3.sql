68404415f7bf7db171b5969c938a786811b647f3
Tarxyn <55942475+Tarxyn@users.noreply.github.com>
2024-02-27 21:23:06 +0300

    Update Script2.3.sql

--- Script2.3.sql
+++ Script2.3.sql
@@ -1,4 +1,3 @@
 -- 3.	Найти сумму часов выходных для разных полов из таблицы HumanResources.Employee.
 
-SELECT SUM(CASE WHEN "Gender" = 'M' THEN "SickLeaveHours" ELSE 0 END) AS "SumTimeMale", SUM(CASE WHEN "Gender" = 'F' THEN "SickLeaveHours" ELSE 0 END) AS "SumTimeFemale"
-FROM "HumanResources"."Employee";
+select "Gender", SUM("SickLeaveHours") AS "SumTime" from "HumanResources"."Employee" group by  "Gender";
