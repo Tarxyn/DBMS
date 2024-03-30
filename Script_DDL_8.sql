43ca402302dabab14fb0c5b802205b8194218fcc
Tarxyn <55942475+Tarxyn@users.noreply.github.com>
2024-03-30 20:19:29 +0300

    Update Script_DDL_8.sql

--- Script_DDL_8.sql
+++ Script_DDL_8.sql
@@ -1,3 +1,6 @@
+-- Написать скрипты на добавление ограничения проверок CHECK для
+-- тех полей, где это необходимо.
+
 ALTER TABLE "Clients"
 ADD CONSTRAINT "CH_Clients_Name"
 CHECK ("Name" is not null);
