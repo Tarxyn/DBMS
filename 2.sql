9436570f6a220d65a46e44df3438ee4b4b14fcad
Tarxyn <55942475+Tarxyn@users.noreply.github.com>
2024-05-22 13:17:09 +0300

    Update 2.sql

--- 2.sql
+++ 2.sql
@@ -1,6 +1,6 @@
 --2.	Выдать ассортимент товара, заказанного данным магазином:
 SELECT p."ID", p."Name", o."Cost", i."Count_item" as quantity
-FROM "Product" p
-JOIN "Item" i ON p."ID" = i."ID_product"
-JOIN "Order" o ON i."ID" = o."ID_Item"
-WHERE o."ID_Item" = <STORE_ID>;
+FROM "Products" p
+JOIN "Items" i ON p."ID" = i."ID_product"
+JOIN "Orders" o ON i."ID" = o."ID_Item"
+WHERE o."ID_Item" = 2;
