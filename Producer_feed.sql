5f486c5f8e4c7c8e090cf6537a3598129ff9a9ac
Tarxyn <55942475+Tarxyn@users.noreply.github.com>
2024-05-22 13:19:46 +0300

    Update Producer_feed.sql

--- Producer_feed.sql
+++ Producer_feed.sql
@@ -1,4 +1,4 @@
 -- Добавить производителей
-INSERT INTO "Producer" ("Name") VALUES ('Производитель 1');
-INSERT INTO "Producer" ("Name") VALUES ('Производитель 2');
-INSERT INTO "Producer" ("Name") VALUES ('Производитель 3');
+INSERT INTO "Producers" ("Name") VALUES ('Производитель 1');
+INSERT INTO "Producers" ("Name") VALUES ('Производитель 2');
+INSERT INTO "Producers" ("Name") VALUES ('Производитель 3');
