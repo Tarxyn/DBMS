94340229d15ea1749c81f89c566462befb7c5725
Tarxyn <55942475+Tarxyn@users.noreply.github.com>
2024-05-24 23:20:16 +0300

    Update 3.sql

--- 3.sql
+++ 3.sql
@@ -1,25 +1,19 @@
 --3.	Показывать список продаж за указанный период времени:
-CREATE OR REPLACE PROCEDURE GetSalesBetweenDates(StartDate date, EndDate date)
-LANGUAGE plpgsql as $$
-begin
-	
-    PERFORM o."ID" as order_id,
-    c."Name" as client_name,
-    p."Name" as product_name,
-    i."Count_item" as quantity,
-    o."Sale_date" as sale_date
-FROM
-    "Orders" o
-JOIN
-    "Clients" c ON o."ID_Client" = c."Name"
-JOIN
-    "Items" i ON o."Vendor_Code" = i."ID"
-JOIN
-    "Products" p ON i."ID_product" = p."Name"
-WHERE
-    o."Sale_date" BETWEEN StartDate AND EndDate;
-
+CREATE OR REPLACE FUNCTION GetSalesBetweenDates(StartDate date, EndDate date)
+RETURNS TABLE (order_id INT, client_name varchar(255), product_name varchar(255), quantity INT, sale_date date) AS $$
+BEGIN
+    RETURN QUERY
+    SELECT o."ID" as order_id,
+           o."ID_Client" as client_name,
+           p."Name" as product_name,
+           i."Count_item" as quantity,
+           o."Sale_date" as sale_date
+    FROM "Orders" o
+    JOIN "Items" i ON o."Vendor_Code" = i."ID"
+    JOIN "Products" p ON i."ID_product" = p."Name"
+    WHERE o."Sale_date" BETWEEN StartDate AND EndDate;
 END;
-$$;
+$$ LANGUAGE plpgsql;
 
-call GetSalesBetweenDates('2023-05-03','2023-05-03');
+-- Вызов функции
+select * from GetSalesBetweenDates('2023-05-03','2023-05-03');
