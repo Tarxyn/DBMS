c258b65750f74cd13e9f0380936d24e66211d820
Tarxyn <55942475+Tarxyn@users.noreply.github.com>
2024-05-24 23:23:20 +0300

    Update Order_feed.sql

--- Order_feed.sql
+++ Order_feed.sql
@@ -1,5 +1,5 @@
 --Добавить заказ
-INSERT INTO "Orders" ("ID_Client", "Vendor_Code", "Cost", "Count", "Max_count", "Sale_date") values ('Клиент 1',3,250,1,2,'2023-05-03');
-INSERT INTO "Orders" ("ID_Client", "Vendor_Code", "Cost", "Count", "Max_count", "Sale_date") values ('Клиент 1',4,250,1,1,'2023-05-02');
-INSERT INTO "Orders" ("ID_Client", "Vendor_Code", "Cost", "Count", "Max_count", "Sale_date") values ('Клиент 1',2,250,2,4,'2023-05-03');
-INSERT INTO "Orders" ("ID_Client", "Vendor_Code", "Cost", "Count", "Max_count", "Sale_date") values ('Клиент 2',5,250,2,4,'2023-05-03');
+INSERT INTO "Orders" ("ID_Client", "Vendor_Code", "Cost", "Count", "Sale_date") values ('Клиент 1',3,250,1,'2023-05-03');
+INSERT INTO "Orders" ("ID_Client", "Vendor_Code", "Cost", "Count", "Sale_date") values ('Клиент 1',4,250,1,'2023-05-02');
+INSERT INTO "Orders" ("ID_Client", "Vendor_Code", "Cost", "Count", "Sale_date") values ('Клиент 1',2,250,2,'2023-05-03');
+INSERT INTO "Orders" ("ID_Client", "Vendor_Code", "Cost", "Count", "Sale_date") values ('Клиент 2',5,250,2,'2023-05-03');
