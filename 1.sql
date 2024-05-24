654cecff71494369eac2b475b202e3c05c5d3e98
Tarxyn <55942475+Tarxyn@users.noreply.github.com>
2024-05-24 23:18:20 +0300

    Update 1.sql

--- 1.sql
+++ 1.sql
@@ -1,11 +1,13 @@
 -- 1.	Выдать ассортимент товара, находящегося на складе сейчас:
-CREATE OR REPLACE PROCEDURE GetAvailableItems(min_quantity INT)
-LANGUAGE plpgsql as $$
-begin
-    PERFORM * 
+CREATE OR REPLACE FUNCTION GetAvailableItems3(min_quantity INT)
+RETURNS SETOF "Items" AS $$
+BEGIN
+    RETURN QUERY
+    SELECT *
     FROM "Items"
     WHERE "Count_item" > min_quantity;
 END;
-$$;
+$$ LANGUAGE plpgsql;
 
-CALL GetAvailableItems(0);
+-- Вызов функции
+select GetAvailableItems3(0);
