eef03befa8e128f65ecee502344035d34b9e809c
Tarxyn <55942475+Tarxyn@users.noreply.github.com>
2024-05-24 11:16:09 +0300

    Update func.sql

--- func.sql
+++ func.sql
@@ -1,17 +1,16 @@
 -- Функция "Сумма заказанных товаров клиента"
 
-CREATE OR REPLACE FUNCTION get_ordered_quantity(client_id INT)
+CREATE OR REPLACE FUNCTION get_ordered_quantity(client_id VARCHAR(255))
 RETURNS INT AS $$
 DECLARE
 total_quantity INT;
 BEGIN
-SELECT SUM(i."Count_item") INTO total_quantity
-FROM "Items" i
-JOIN "Orders" o ON i."ID" = o."ID_Item"
-WHERE o."ID_Item" = get_ordered_quantity.client_id; -- явное указание параметра функции
+SELECT SUM(o."Count") INTO total_quantity
+FROM "Orders" o
+WHERE o."ID_Client" = get_ordered_quantity.client_id; -- явное указание параметра функции
 RETURN total_quantity;
 END;
 $$ LANGUAGE plpgsql;
 
 -- Вызов функции
-SELECT get_ordered_quantity(5);
+SELECT get_ordered_quantity('Клиент 1');
