8056451fc1d26e6f659f4ba6b1fb61ed08c7b328
Tarxyn <55942475+Tarxyn@users.noreply.github.com>
2024-05-24 11:13:59 +0300

    Update Item_feed.sql

--- Item_feed.sql
+++ Item_feed.sql
@@ -1,6 +1,7 @@
 -- Добавить товары
-INSERT INTO "Items" ("Count_item", "ID_product", "ID_producer") VALUES (1, 2, 1);
-INSERT INTO "Items" ("Count_item", "ID_product", "ID_producer") VALUES (2, 1, 1);
-INSERT INTO "Items" ("Count_item", "ID_product", "ID_producer") VALUES (3, 3, 2);
-INSERT INTO "Items" ("Count_item", "ID_product", "ID_producer") VALUES (4, 3, 2);
-INSERT INTO "Items" ("Count_item", "ID_product", "ID_producer") VALUES (15, 2, 2);
+INSERT INTO "Items" ("Count_item", "ID_product", "ID_producer") VALUES (1, 'Товар 2', 'Производитель 1');
+INSERT INTO "Items" ("Count_item", "ID_product", "ID_producer") VALUES (2, 'Товар 1', 'Производитель 1');
+INSERT INTO "Items" ("Count_item", "ID_product", "ID_producer") VALUES (3, 'Товар 3', 'Производитель 2');
+INSERT INTO "Items" ("Count_item", "ID_product", "ID_producer") VALUES (4, 'Товар 3', 'Производитель 2');
+INSERT INTO "Items" ("Count_item", "ID_product", "ID_producer") VALUES (15, 'Товар 2', 'Производитель 2');
+INSERT INTO "Items" ("Count_item", "ID_product", "ID_producer") VALUES (0, 'Товар 3', 'Производитель 3');
