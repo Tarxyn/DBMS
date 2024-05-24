798d794d052ca2735857b9f95a1204aeab9e1829
Tarxyn <55942475+Tarxyn@users.noreply.github.com>
2024-05-24 11:14:42 +0300

    Update Order_create_table.sql

--- Order_create_table.sql
+++ Order_create_table.sql
@@ -1,35 +1,10 @@
 -- таблица "Заказ"
 create table "Orders" (
 	"ID" SERIAL PRIMARY KEY
-	,"ID_Client" INT NOT NULL
-	,"ID_Item" INT NOT NULL
+	,"ID_Client" VARCHAR(255) NOT NULL
+	,"Vendor_Code" INT NOT NULL
 	,"Cost" DECIMAL(10, 2) NOT NULL
 	,"Count" INT NOT null
 	,"Max_count" Int not null
 	,"Sale_date" DATE NOT NULL
 );
-
-
--- Создание внешнего ключа между двумя таблицами
-ALTER TABLE "Orders"
-ADD CONSTRAINT FK_Orders_Item_Count
-FOREIGN KEY ("Max_count")
-REFERENCES "Items"("Count_item");
-
--- Создание внешнего ключа между двумя таблицами
-ALTER TABLE "Orders"
-ADD CONSTRAINT FK_Orders_Item_ID
-FOREIGN KEY ("ID_Item")
-REFERENCES "Items"("ID");
-
--- Создание внешнего ключа между двумя таблицами
-ALTER TABLE "Orders"
-ADD CONSTRAINT FK_Orders_Clients
-FOREIGN KEY ("ID_Client")
-REFERENCES "Clients"("ID");
-
-
---Создание условия что кол-во в заказе не должно быть меньше 0 и более чем на складе
-ALTER TABLE "Orders"
-ADD CONSTRAINT CH_Orders_Count
-CHECK ("Count" >= 0 AND "Count" <= "Max_count");
